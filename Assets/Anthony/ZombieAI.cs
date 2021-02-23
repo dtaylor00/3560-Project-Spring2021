23d84b213fbe27264f0f21bdc856dd56dbeab7ae
Anthony Giordano <59939634+Anthony3714@users.noreply.github.com>
2021-02-23 17:21:46 -0500

    Improving Zombie AI

--- Assets/Anthony/ZombieAI.cs
+++ Assets/Anthony/ZombieAI.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class ZombieAI : MonoBehaviour
 {
     NavMeshAgent nm;    
@@ -10,27 +9,39 @@ public class ZombieAI : MonoBehaviour
     public enum AIState {chasing, attack};
     public AIState aiState;
     public float speed;
-
-
     //public AIState aiState;
     // Start is called before the first frame update
     void Start()
     {
         nm = GetComponent<NavMeshAgent>(); 
+        aiState = AIState.chasing;
+        target = GameObject.FindGameObjectWithTag("Player").transform;
         StartCoroutine(Think());
     }
     IEnumerator Think(){
         while(true){
+            if(nm.speed < 20){
+                nm.speed = 5+Mathf.Sqrt(Time.time);
+            }
+            float dist = Vector3.Distance(target.position, transform.position);
+            if(dist < 2){
+                aiState = AIState.attack;
+            }
+            else{
+                aiState = AIState.chasing;
+            }
             switch(aiState){
+
                 case AIState.chasing:
                     nm.SetDestination(target.position);
-                    nm.speed = speed;
                     break;
                 case AIState.attack:
+
                     break;
                 default:
                     break;
             }
+
             yield return new WaitForSeconds(1f);
         }
     }
