a516bc0d66423f581149a5fede2b0870954088ba
Gyver <54608994+Gyver-Blessing@users.noreply.github.com>
2021-03-01 15:55:13 -0500

    I think universal powerup script works

--- Assets/Gyver/Power_Up.cs
+++ Assets/Gyver/Power_Up.cs
@@ -7,7 +7,7 @@ public class Power_Up : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player"){
-            if(other.gameObject.name.Equals("JumpPowerUp")){
+            if(this.gameObject.name.Equals("JumpPowerUp")){
                 other.gameObject.GetComponent<Movement_FPS>().jumpHeight = 20f;
                 Destroy(this.gameObject);
             }
